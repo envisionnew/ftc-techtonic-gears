57208806b6401aee83887ace8093b4d182c54705
SaurishS <saurishs1215@gmail.com>
2017-10-21 16:26:08 -0700

    Updated RelicArm v 1.1
    Updated RelicArm v 1.1

--- Code/RelicArm.java
+++ Code/RelicArm.java
@@ -1,74 +1,37 @@
-package org.techtonicgears.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.org.techtonicgrars.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
+public class RelicArm {
+    public DcMotor relicSlide   = null;
+    public Servo relicClaw = null;
+    public Servo relicArm1 = null;
+    public Servo relicArm2 = null;
+    public final double relicArm1_ST = 0.5;
+    public final double relicArm2_ST = 0.5;
+    public final double relicClaw_ST = 0.5;
+    HardwareMap hwMap = null;
 
-@TeleOp(name = "TeleOp: Relic")
-@Disabled
-public class RelicTest extends OpMode{
-    RelicArm arm = new RelicArm();
-    DcMotor slide;
-    Servo arm1;
-    Servo arm2;
-    Servo claw;
-    double linearM = 0.0d;
-    double clawPos = 0.0d;
-    double arm1Pos = 0.0d;
-    double arm2Pos = 0.0d;
-    @Override
-    public void init() {
-        arm.init(hardwareMap);
-        telemetry.addData("", "Press Start");
-        telemetry.update();
-    }
-    @Override
-    public void init_loop(){
-    }
-    @Override
-    public void start() {
+    private ElapsedTime period  = new ElapsedTime();
 
-    }
-    @Override
-    public void loop() {
+    public void init(HardwareMap Map) {
+        hwMap = Map;
+        relicSlide = hwMap.get(DcMotor.class, "Relic_Slide");
+        relicClaw = hwMap.get(Servo.class, "Relic_Claw");
+        relicArm1 = hwMap.get(Servo.class, "Relic_Arm_1");
+        relicArm2 = hwMap.get(Servo.class, "Relic_Arm_2");
+        relicSlide.setDirection(DcMotor.Direction.FORWARD);
+        relicClaw.setPosition(relicClaw_ST);
+        relicArm1.setPosition(relicArm1_ST);
+        relicArm2.setPosition(relicArm2_ST);
+        relicSlide.setPower(0);
+        relicSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        slide = arm.relicSlide;
-        arm1 = arm.relicArm1;
-        arm2 = arm.relicArm2;
-        claw = arm.relicClaw;
-
-        linearM = gamepad2.left_stick_y/4;
-        if(gamepad2.right_trigger > 0){
-            clawPos += 0.01d;
-        }else if(gamepad2.left_trigger > 0){
-            clawPos -= 0.01d;
-        }
-        if(gamepad2.y){
-            arm1Pos += 0.02d;
-        }else if(gamepad2.a){
-            arm1Pos -= 0.02d;
-        }
-        if(gamepad2.x){
-            arm1Pos += 0.02d;
-        }else if(gamepad2.b){
-            arm1Pos -= 0.02d;
-        }
-        arm2Pos = Range.clip(arm2Pos, -0.5, 0.5);
-        clawPos = Range.clip(clawPos, -0.5, 0.5);
-        arm1Pos = Range.clip(arm1Pos, -0.5, 0.5);
-
-        slide.setPower(linearM);
-        claw.setPosition(arm.relicClaw_ST+clawPos);
-        arm1.setPosition(arm.relicArm1_ST+arm1Pos);
-        arm2.setPosition(arm.relicArm2_ST+arm2Pos);
-
-    }
-    @Override
-    public void stop() {
     }
 }
