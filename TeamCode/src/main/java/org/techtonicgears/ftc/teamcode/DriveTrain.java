abbde3a72ff6703ec2ab36c38a5ff3dc44b8954a
RitaliJ <ritaliaarav@gmail.com>
2017-12-17 21:26:58 -0800

    minor cleanup of code

--- TeamCode/src/main/java/org/techtonicgears/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/techtonicgears/ftc/teamcode/DriveTrain.java
@@ -1,29 +1,23 @@
 package org.techtonicgears.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 
 public class DriveTrain {
     DcMotor leftFront   = null;
     DcMotor rightFront   = null;
     DcMotor  leftBack  = null;
     DcMotor  rightBack  = null;
-    double speed;
-    double offset;
-    HardwareMap hwMap = null;
 
-    public void init(HardwareMap Map) {
-        hwMap = Map;
-        leftFront = hwMap.get(DcMotor.class, "front_left");
-        rightFront = hwMap.get(DcMotor.class, "front_right");
+
+    public void init(HardwareMap map) {
+
+        leftFront = map.get(DcMotor.class, "front_left");
+        rightFront = map.get(DcMotor.class, "front_right");
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack = hwMap.get(DcMotor.class, "rear_left");
-        rightBack = hwMap.get(DcMotor.class, "rear_right");
+        leftBack = map.get(DcMotor.class, "rear_left");
+        rightBack = map.get(DcMotor.class, "rear_right");
         leftBack.setDirection(DcMotor.Direction.FORWARD);
         rightBack.setDirection(DcMotor.Direction.REVERSE);
         leftFront.setPower(0);
@@ -32,11 +26,13 @@ public class DriveTrain {
         rightBack.setPower(0);
         leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
     }
-    public void move(double power, double dif){
-        speed = power;
-        offset = dif;
+    public void move(double speed, double offset){
+
         leftFront.setPower(speed-offset);
         rightFront.setPower(speed+offset);
         leftBack.setPower(speed-offset);
