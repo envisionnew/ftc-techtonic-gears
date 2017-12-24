e774d10f923efc8523fd1c434843abd513794445
mujoo <shuhulmujoo@gmail.com>
2017-12-23 17:04:28 -0800

    Mechanum 1.0

--- TeamCode/src/main/java/org/techtonicgears/ftc/teamcode/MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/techtonicgears/ftc/teamcode/MecanumDriveTrain.java
@@ -14,31 +14,41 @@ public class MecanumDriveTrain {
 
         leftFront = map.get(DcMotor.class, "front_left");
         rightFront = map.get(DcMotor.class, "front_right");
+
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
+
         leftBack = map.get(DcMotor.class, "rear_left");
         rightBack = map.get(DcMotor.class, "rear_right");
+
         leftBack.setDirection(DcMotor.Direction.FORWARD);
         rightBack.setDirection(DcMotor.Direction.REVERSE);
+
         leftFront.setPower(0);
         rightFront.setPower(0);
         leftBack.setPower(0);
         rightBack.setPower(0);
+
         leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
     }
     //the controls give reverse values, thus, we are subtracting powers in the left to turn right
     //since it actually shows up as a negative power
-    public void move(double speed, double offset, double strafe){
+    public void move(double speed, double strafe, double offset){
 
-        leftFront.setPower(speed-offset-strafe);
-        rightFront.setPower(speed+offset+strafe);
-        leftBack.setPower(speed-offset+strafe);
-        rightBack.setPower(speed+offset-strafe);
+        leftFront.setPower(speed-strafe-offset);
+        rightFront.setPower(speed+strafe+offset);
+        leftBack.setPower(-speed-strafe+offset);
+        rightBack.setPower(-speed+strafe-offset);
     }
 
     //strafing is on right joystick x axis along with the regular tank motion of forward and back(y axis)
