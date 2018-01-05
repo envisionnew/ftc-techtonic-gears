f4c66d67328f88db831a4cb8e9d0c6c04b7814bb
SaurishS <saurishs1215@gmail.com>
2018-01-05 11:16:09 -0800

    Better Comments and Cleaned Up Code

--- TeamCode/src/main/java/org/techtonicgears/ftc/teamcode/JewelArm.java
+++ TeamCode/src/main/java/org/techtonicgears/ftc/teamcode/JewelArm.java
@@ -1,18 +1,31 @@
 package org.techtonicgears.ftc.teamcode;
+
+/*/ Imports /*/
+
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
 public class JewelArm {
-    //defining the jewel arm
+
+    /*/ Defining the Jewel Arm /*/
+
     Servo jewelArm;
     HardwareMap hwMap;
     public void init(HardwareMap Map){
         hwMap = Map;
+
+        /*/ Naming the Jewel Arm /*/
+
         jewelArm = hwMap.get(Servo.class, "jewel_arm");
+
+        /*/ Setting the position of the Jewel Arm  upon initialize. */
+
         jewelArm.setPosition(0);
     }
-    //method for setting jewel arm to a certain position, parameter is a double which is used to set the position
+
+    /*/ Method for setting the Jewel Arm to a certain position. Pos is a double which is used to set the position of the Jewel Arm. /*/
+
     public void setJewelArm(double Pos){
         jewelArm.setPosition(Pos);
     }
