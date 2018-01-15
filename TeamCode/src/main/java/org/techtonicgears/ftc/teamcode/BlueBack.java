8d2d75acaab45822b6f0732d6b2f03b7dd08d7e3
mujoo <shuhulmujoo@gmail.com>
2018-01-15 12:25:51 -0800

    Almost Finished AutoBlueBack 2.0

--- TeamCode/src/main/java/org/techtonicgears/ftc/teamcode/BlueBack.java
+++ TeamCode/src/main/java/org/techtonicgears/ftc/teamcode/BlueBack.java
@@ -1,14 +1,15 @@
 package org.techtonicgears.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.*;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
-import com.vuforia.Vuforia;
 
-@Autonomous(name = "RedBack")
-public class RedBack extends LinearOpMode {
+
+@Autonomous(name = "Blueback")
+public class BlueBack extends LinearOpMode {
     //Declaring glyph arm
     GlyphArm glyphArm = new GlyphArm();
     //Declaring Drive train
@@ -22,12 +23,11 @@ public class RedBack extends LinearOpMode {
     double left = 0.5;
     double middle = 1;
     double right = 0;
-    String teamColor = "red";
+    String teamColor = "blue";
     String foundColor = "";
 
     ModernRoboticsI2cGyro gyro;
     ColorSensor colorSensor;
-    Vuforia vuforia = new Vuforia();
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -41,11 +41,12 @@ public class RedBack extends LinearOpMode {
 
 
         //Resetting the timer
+        jewelArm.setJewelArm(0);
 
 
         gyro.calibrate();
-        while (!isStopRequested() && gyro.isCalibrating())  {
-            telemetry.addData("Calibrating","");
+        while (!isStopRequested() && gyro.isCalibrating()) {
+            telemetry.addData("Calibrating", "");
             telemetry.update();
             sleep(50);
         }
@@ -65,7 +66,7 @@ public class RedBack extends LinearOpMode {
         colorSensor.enableLed(true);
 
         while (opModeIsActive() && timer.seconds() < 2) {
-            jewelArm.setJewelArm(0.65);
+            jewelArm.setJewelArm(1);
             //returns which color the jewel is
             if (colorSensor.red() > colorSensor.blue()) {
                 foundColor = "red";
@@ -78,36 +79,68 @@ public class RedBack extends LinearOpMode {
             telemetry.addData("Blue", "");
             telemetry.update();
             while (opModeIsActive() && timer.seconds() < 0.3) {
-                jewelArm.setJewelArm(0.65);
+                jewelArm.setJewelArm(1);
                 //moves and pushes the other jewel
                 driveTrain.move(0, 0.5, 0);
             }
+            jewelArm.setJewelArm(0);
             // if the color of the jewel is not the same as the color of the team
 
             timer.reset();
-            while (opModeIsActive() && gyro.getHeading() < 359){
-                driveTrain.move(0, -0.4, 0);
+            while (opModeIsActive() && gyro.getHeading() < 359) {
+                driveTrain.move(0, -0.25, 0);
                 jewelArm.setJewelArm(0);
             }
 
 
-        } else if (foundColor != teamColor ){
+        } else if (foundColor != teamColor) {
             telemetry.addData("Red", "");
             telemetry.update();
             while (opModeIsActive() && timer.seconds() < 0.3) {
-                jewelArm.setJewelArm(0.65);
+                jewelArm.setJewelArm(1);
                 //moves and pushes off the same jewel it detects because colors dont match
                 driveTrain.move(0, -0.5, 0);
             }
-            while (opModeIsActive() && gyro.getHeading() > 1){
-                driveTrain.move(0, 0.4,0);
+            jewelArm.setJewelArm(0);
+            while (opModeIsActive() && gyro.getHeading() > 1) {
+                driveTrain.move(0, 0.25, 0);
             }
         }
-        /*
-        while (opModeIsActive() && timer.seconds() < 1){
+
+        gyro.resetZAxisIntegrator();
+
+        telemetry.addData("gyro", gyro.getHeading());
+        telemetry.update();
+
+        timer.reset();
+        while (opModeIsActive() && timer.seconds() < 0.2){
+            driveTrain.move(0, -0.25,0);
+        }
+
+        while (opModeIsActive() && gyro.getHeading() < 90){
+            driveTrain.move(0, -0.25,0);
+        }
+
+        timer.reset();
+        while (opModeIsActive() && timer.seconds() < 3){
             driveTrain.move(0.5, 0,0);
         }
-        */
+
+        gyro.resetZAxisIntegrator();
+
+        timer.reset();
+        while (opModeIsActive() && timer.seconds() < 0.2){
+            driveTrain.move(0, -0.25,0);
+        }
+
+        while (opModeIsActive() && gyro.getHeading() < 90){
+            driveTrain.move(0, -0.25,0);
+        }
+
+
+
+
+
 
     }
 }
